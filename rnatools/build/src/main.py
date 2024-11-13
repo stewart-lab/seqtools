ed4c66bcca2e7a1987379d9120fcff5775140e08
Robert Millikin <rmillikin@wisc.edu>
2024-11-12 21:03:06 -0600

    add rsem alignment summary

--- rnatools/build/src/main.py
+++ rnatools/build/src/main.py
@@ -28,11 +28,14 @@ def main():
     _trim_fastqs(fastq_dir, fastp_dir)
 
     # summarize fastp reports
-    _summarize_fastp_reports(fastp_dir, fastp_dir)
+    _summarize_fastp_reports(fastp_dir, output_dir)
 
     # run rsem on trimmed fastqs
     rsem_dir = os.path.join(output_dir, '002_rsem')
     _run_rsem(fastp_dir, rsem_dir, rsem_reference)
+
+    # summarize fastp reports again, this time with RSEM counts
+    _summarize_fastp_reports(fastp_dir, output_dir, rsem_dir)
 
     # get counts matrix from RSEM output
     counts_dir = os.path.join(output_dir, '003_count_matrix')
@@ -73,9 +76,13 @@ def _trim_fastqs(input_dir: str, output_dir: str) -> None:
     # zip up the .html files for download
     # os.system(f'zip -r {output_dir}/__fastp_html_reports.zip {output_dir}/*.html')
 
-def _summarize_fastp_reports(fastp_dir: str, output_dir: str) -> None:
+def _summarize_fastp_reports(fastp_dir: str, output_dir: str, rsem_dir: str = "") -> None:
+    if rsem_dir:
+        aligned_counts = _read_rsem_counts(rsem_dir)
+
     os.makedirs(output_dir, exist_ok=True)
     fastp_reports = [f for f in os.listdir(fastp_dir) if f.endswith('.json')]
+    fastp_reports.sort()
 
     read_counts_per_sample = dict()
 
@@ -91,14 +98,27 @@ def _summarize_fastp_reports(fastp_dir: str, output_dir: str) -> None:
             
             # sample name is just the file name without the .json extension
             sample_name = report.replace('.json', '')
+
             read_counts_per_sample[sample_name] = {
                 'total_reads_before_filtering': total_reads_before,
                 'total_reads_after_filtering': total_reads_after,
                 'low_quality_reads': low_quality_reads
             }
 
+            # add the counts to the dictionary
+            if rsem_dir:
+                sample_name = report.replace('.json', '')
+                
+                if sample_name in aligned_counts:
+                    read_counts_per_sample[sample_name]['aligned_reads'] = aligned_counts[sample_name]
+                else:
+                    print(f'Error: no RSEM counts found for {sample_name}')
+
     # write the summary to a .txt file
-    summary_path = os.path.join(output_dir, '__fastp_summary.txt')
+    if rsem_dir:
+        summary_path = os.path.join(output_dir, '__fastp_rsem_summary.txt')
+    else:
+        summary_path = os.path.join(output_dir, '__fastp_summary.txt')
 
     with open(summary_path, 'w') as f:
         for sample, counts in read_counts_per_sample.items():
@@ -106,7 +126,18 @@ def _summarize_fastp_reports(fastp_dir: str, output_dir: str) -> None:
             f.write("Total reads before filtering: " + str(counts['total_reads_before_filtering']) + "\n")
             f.write("Total reads after filtering: " + str(counts['total_reads_after_filtering']) + "\n")
             f.write("Low quality reads: " + str(counts['low_quality_reads']) + "\n")
-            f.write("Percent of reads kept: " + str(counts['total_reads_after_filtering'] / counts['total_reads_before_filtering']) + "\n")
+
+            percentage_reads_kept = (counts['total_reads_after_filtering'] / counts['total_reads_before_filtering']) * 100
+            percentage_reads_kept = round(percentage_reads_kept, 2)
+            f.write("Percent of reads kept: " + str(percentage_reads_kept) + "%\n")
+
+            if 'aligned_reads' in counts:
+                f.write("Aligned reads: " + str(counts['aligned_reads']) + "\n")
+
+                # calculate percentage rounded to 2 decimal places
+                percentage_aligned = (counts['aligned_reads'] / counts['total_reads_after_filtering']) * 100
+                percentage_aligned = round(percentage_aligned, 2)
+                f.write("Percentage of reads aligned: " + str(percentage_aligned) + "%\n")
 
             # add a bar chart of the before/after filtering read counts
             before_filtering_bar_count = int(counts['total_reads_before_filtering'] / 2000000)
@@ -124,6 +155,12 @@ def _summarize_fastp_reports(fastp_dir: str, output_dir: str) -> None:
 
             f.write("Bar chart of reads before filtering: ---> " + before_bar + "\n")
             f.write("Bar chart of reads after filtering:  ---> " + after_bar + "\n")
+
+            if 'aligned_reads' in counts:
+                aligned_bar_count = int(counts['aligned_reads'] / 2000000)
+                aligned_bar = "#" * aligned_bar_count
+                aligned_bar = ' '.join([aligned_bar[i:i+10] for i in range(0, len(aligned_bar), 10)])
+                f.write("Bar chart of reads aligned:          ---> " + aligned_bar + "\n")
 
             f.write("\n\n")
 
@@ -155,6 +192,37 @@ def _get_counts_csv(rsem_dir: str, output_dir: str) -> str:
     os.makedirs(output_dir)
     # build the counts.csv file from the output in the RSEM directory
     os.system(f'Rscript /src/tximport.R --rsem_dir {rsem_dir} --output_dir {output_dir}')
+
+def _read_rsem_counts(rsem_dir: str) -> dict:
+    rsem_alignment_counts = dict()
+
+    # get all dirs that end in .stat
+    stat_dirs = [f for f in os.listdir(rsem_dir) if f.endswith('.stat')]
+    stat_dirs.sort()
+    for stat_dir in stat_dirs:
+        stat_dir_path = os.path.join(rsem_dir, stat_dir)
+
+        # sample name is just removing the .stat extension
+        sample_name = stat_dir.replace('.stat', '')
+
+        # read the .cnt file in the .stat directory
+        cnt_file = [f for f in os.listdir(stat_dir_path) if f.endswith('.cnt')]
+
+        if not cnt_file:
+            print(f'Error: no .cnt file found in {stat_dir_path}')
+            continue
+
+        cnt_file_path = os.path.join(stat_dir_path, cnt_file[0])
+
+        with open(cnt_file_path, 'r') as f:
+            line0 = f.readline()
+            # split by space
+            split_line0 = line0.split()
+            unalignable = int(split_line0[0]) * 2   # multiply by 2 because paired end
+            alignable = int(split_line0[1]) * 2     # multiply by 2 because paired end
+            rsem_alignment_counts[sample_name] = alignable
+    
+    return rsem_alignment_counts
 
 def _make_output_dir(output_dir: str) -> str:
     current_datetime = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
